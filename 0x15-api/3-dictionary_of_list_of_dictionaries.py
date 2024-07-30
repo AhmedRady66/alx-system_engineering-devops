a0972960ab472e3a9869bf9cbcb816510e119c04
Ahmed Rady <rady20991@gmail.com>
2024-07-30 03:24:21 +0300

    Dictionary of list of dictionaries

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -3,9 +3,36 @@
 
 import json
 import requests
-import sys
+
+
+def fetch_user_data():
+    """Get user information and to-do lists for all employee"""
+    url = "https://jsonplaceholder.typicode.com/"
+
+    users = requests.get(url + "users").json()
+
+    data_export = {}
+
+    for user in users:
+        user_id = user["id"]
+        todos_response = requests.get(url + f"todos?userId={user_id}")
+        todo_list = todos_response.json()
+
+        data_export[user_id] = []
+
+        for todo in todo_list:
+            task_info = {
+                "task": todo.get("title"),
+                "completed": todo.get("completed"),
+                "username": user.get("username")
+            }
+
+            data_export[user_id].append(task_info)
+
+    return data_export
 
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user_id = sys.argv[1]
+    data_export = fetch_user_data()
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump(data_export, jsonfile, indent=4)
