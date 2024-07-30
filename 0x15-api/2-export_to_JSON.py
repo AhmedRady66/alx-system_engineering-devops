1dbc1b3ad0c1862c4f16ddbeaf23cbd3f7ba6eb2
Ahmed Rady <rady20991@gmail.com>
2024-07-30 03:04:09 +0300

    Export to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,38 +1,32 @@
 #!/usr/bin/python3
 """extend your Python script to export data in the JSON format."""
 
+
 import json
 import requests
 import sys
 
+
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
-
     user_id = sys.argv[1]
+
     user = requests.get(url + "users/{}".format(user_id)).json()
     username = user.get("username")
 
     params = {"userId": user_id}
-    todos = requests.get(url + "todos", params=params).json()
+    todos = requests.get(url + "todos", params).json()
 
-    # Check if user_id is a list of dictionaries
-    if isinstance(todos, list) and all(isinstance(i, dict) for i in todos):
-        print("USER_ID's value type is a list of dicts: OK")
-
-        data_export = {user_id: []}
-
-        for i in todos:
-            task_info = {
+    data_export = {
+        user_id: [
+            {
                 "task": i.get("title"),
                 "completed": i.get("completed"),
                 "username": username
             }
-            data_export[user_id].append(task_info)
+            for i in todos
+        ]
+    }
 
-        if len(data_export[user_id]) == len(todos):
-            print("All tasks found: OK")
-
-        with open("{}.json".format(user_id), "w") as jsonfile:
-            json.dump(data_export, jsonfile, indent=4)
-    else:
-        print("USER_ID's value is not a list of dicts")
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump(data_export, jsonfile, indent=4)
